eb32cfd9064ece101b6fe1cd54ae0593a2e2337e
Ruby Rubenstahl <ruby@ruby-lighting.com>
2017-09-04 18:48:25 -0400

    Inline SVG remove test

--- src/Dipswitch.js
+++ src/Dipswitch.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-
-const Switch = (props) => {
-  const { on = false,
-    label = "1",
-    width = 12,
-    onClick,
-    switchColor = '#FFF',
-    channelColor = '#222',
-    bodyColor = '#3768b2',
-    labelColor = '#FFF'
-  } = props;
-
-  const height = width * 4;
-  const fontSize = width / 1.1;
-  const switchPosition = on ? '24%' : '50%';
-
-  return (
-      <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} onClick={onClick}>
-        <rect width="100%" height="100%" fill={bodyColor} />
-        <rect width="66%" height="40%" x="16%" y="24%" fill={channelColor} />
-        <rect width="66%" height="16%" x="17%" y={switchPosition} fill={switchColor} />
-        <text x="50%" y="92%" fontSize={fontSize} fill={labelColor} textAnchor="middle">{label}</text>
-      </svg>
-  )
-};
-
+import Switch from './Switch';
 
 class Dipswitch extends Component {
   constructor(props) {
@@ -70,7 +44,8 @@ class Dipswitch extends Component {
 
     iterator(switchCount, i => {
       const switchState = Math.pow(2, i) & value;
-      const s = (<Switch
+      const s = (
+          <Switch
               key={i}
               label={i + 1}
               onClick={this.onSwitchClick(i)}
@@ -79,7 +54,6 @@ class Dipswitch extends Component {
               switchColor={switchColor}
               channelColor={channelColor}
               bodyColor={bodyColor}
-              labelColor={labelColor}
           />);
       switches.push(s);
     })
@@ -103,8 +77,6 @@ Dipswitch.PropTypes = {
   bodyColor: PropTypes.string,
   labelColor: PropTypes.string,
 }
-
-
 
 
 const msbRightIterator = (count, fn) => {
